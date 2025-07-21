7c9b4171f4026dd9495153c37be6c93efb3d19bb
Keveray <154699558+Keveray@users.noreply.github.com>
2025-07-21 22:45:10 +0300

    v2

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,28 @@ import (
 	"github.com/Keveray/VKinternship/pkg/handler"
 	"github.com/Keveray/VKinternship/pkg/repository"
 	"github.com/Keveray/VKinternship/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(VKinternship.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error acucured while running http server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	viper.SetEnvPrefix("APP")
+	viper.AutomaticEnv()
+	return viper.ReadInConfig()
 }
